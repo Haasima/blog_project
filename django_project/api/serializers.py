f6a500da860c22e4429ae5819490b934d69b1943
wikiwoo <wikiwoo@wikiwoos-MacBook-Pro.local>
2023-12-04 05:45:51 -0800

    first

---
+++ django_project/api/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from blog.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author']
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'posts']
---
+++ django_project/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer, PostSerializer
+from django.contrib.auth.models import User
+from blog.models import Post
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    
